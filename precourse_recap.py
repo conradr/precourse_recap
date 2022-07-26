a2d15c706f8f3e358cf0a3f3348287e4b734e00b
Conrad Russo <conradr@gmail.com>
2022-07-26 09:49:05 +0100

    added format method

--- precourse_recap.py
+++ precourse_recap.py
@@ -4,5 +4,8 @@ num2 = 6.3
 # Add two numbers
 sum = num1 + num2
 
-# Display the sum
+# Display the sum using format method
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+# Display using string interprolation
 print(f'The sum of {num1} and {num2} is {sum}')
